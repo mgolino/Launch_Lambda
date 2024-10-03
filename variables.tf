e56a27bcf1048106bac8a6cb348d5d7a843e68e9
Mark Golino <markvpr@gmail.com>
2024-10-03 13:48:53 -0400

    Update and rename variables.MPG to variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "subnet_ids" {
+  default = "subnet-0e10efe028772a50d"
+}
+
+variable "security_group_ids" {
+ default = "sg-0f54cfef2abad9ffe"
+}
+
+
+
+
+/* variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+  default     = "t2.medium"
+}*/
