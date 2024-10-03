a89a2ddc6ac3408eaabdba04451e5ca3be754747
Mark Golino <markvpr@gmail.com>
2024-10-03 13:41:09 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_lambda_function" "lambda" {
     filename = "lambda_function_src.zip"
     function_name = "python_terraform_lambda"
     role = aws_iam_role.iam_for_lambda.arn
-    
+    vpc_id = aws_vpc.lookup.id
     source_code_hash = data.archive_file.lambda.output_base64sha256
 
     runtime = "python3.10"
