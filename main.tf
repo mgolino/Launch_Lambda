59d3051e2aeff19042c2eb94efa46eeb5f44ee59
Mark Golino <markvpr@gmail.com>
2024-10-03 12:34:05 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,34 @@
+data "aws_iam_policy_document" "assume_role" {
+    statement {
+        effect = "Allow"
+
+        principals {
+            type = "Service"
+            identifiers = ["lambda.amazonaws.com"]
+        }
+
+        actions = ["sts:AssumeRole"]
+    }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+    name = "iam_for_lambda"
+    assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "archive_file" "lambda" {
+    type = "zip"
+    source file = "${path.module}/../src/lambda.py"
+    output_path = "lambda_function_src.zip"
+}
+
+resource "aws_lambda_function" "lambda" {
+    filename = "lambda_function_src.zip"
+    function_name = "python_terraform_lambda"
+    role = aws_iam_role.iam_for_lambda.arn
+
+    source_code_hash = data.archive_file.lambda.output_base64sha256
+
+    runtime = "python3.10"
+    handler = "lambda_handler"
+}
