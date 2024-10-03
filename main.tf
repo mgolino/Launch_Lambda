dc1a4543caaa3c32b9d6c7afa5438ba5aeef2335
Mark Golino <markvpr@gmail.com>
2024-10-03 11:37:13 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "aws_lambda_function" "example" {
   # filename      = "lambda_function_payload.zip"  # Your Lambda function code
   function_name = "MyLambdaFunction"
-  role          = aws_iam_role.lambda_role.arn
+  role          = aws_iam_role.Lambda-Role-MPG.arn
   handler       = "exports.handler"  # This depends on your runtime
 
-  source_code_hash = filebase64sha256("lambda_function_payload.zip")
+  # source_code_hash = filebase64sha256("lambda_function_payload.zip")
 
   runtime = "python3.12"
   # Other configurations like memory size, timeout, etc., can be added here
