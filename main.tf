47a72e37fa3d22414c96d3dd40f572bfa3220455
Mark Golino <markvpr@gmail.com>
2024-10-03 11:45:11 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
 resource "aws_lambda_function" "example" {
   # filename      = "lambda_function_payload.zip"  # Your Lambda function code
   function_name = "MyLambdaFunction"
