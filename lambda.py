3a739aefc498783a62e737ff6f3b5f872643edfd
Mark Golino <markvpr@gmail.com>
2024-10-04 13:30:19 -0400

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -1,31 +1,15 @@
 import json
 import boto3
 
-ec2 = boto3.resource('ec2')
-# instance_name = 'ec2-mpg-server'
-
-# instance_id = None
-
-ec2_list = ec2.instances.all()
-instance_exists = True
-
 def lambda_handler(event, context):
-    for instance in ec2_list:
-        for tag in instance.tags:
-            if tag['Key'] =='Environment' and tag['Value'] == 'Dev':
-                instance_exists = True
-                instance_id = instance.id
-                ec2.Instance(instance_id).stop()
-                print(f"An instance named '{instance_id}' has been stopped .")
-#                break
-#            if instance_exists:
-#                break
+    client = boto3.client('s3')
+    response = client.create_bucket(
+        Bucket='mpgtestbucket123',
+        )   
     
-#   print(f" {instance_id}")
-
-
-
-    return {
-        'statusCode': 200,
-        'body': json.dumps('EC2 stop script')
-    }
+    
+    
+#    return {
+#        'statusCode': 200,
+#        'body': json.dumps('Hello from Lambda!')
+#   }
