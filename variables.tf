91cb4a36813abe7c5cc4571ea3bf7800f81cd61b
Mark Golino <markvpr@gmail.com>
2024-10-04 14:36:09 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-variable "subnet_ids" {
+/*variable "subnet_ids" {
   default = "subnet-0baaac8c622257ef8"
 }
 
@@ -6,12 +6,12 @@ variable "security_group_ids" {
  default = "sg-02187bbed09dbd891"
 }
 
-/*variable "vpc_id" {
+variable "vpc_id" {
 default = "vpc-0f5ebef494a1a8c30"
-}*/
+}
 
 
-/* variable "instance_type" {
+ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t2.medium"
 }*/
